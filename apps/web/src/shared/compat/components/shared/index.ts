82e4d892ec812de6dc5b793700e956898b2887b7
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-13 14:13:35 -0300

    chore: remove compat copies

--- apps/web/src/shared/compat/components/shared/index.ts
+++
@@ -1,5 +0,0 @@
-export { KPICard } from "./KPICard";
-export { SearchInput } from "./SearchInput";
-export { ActionDropdown } from "./ActionDropdown";
-export { PageHeader } from "./PageHeader";
-export { FilterSelect } from "./FilterSelect";
